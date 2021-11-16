d97eb07a294b56a6f1159adf1b0c60b8547f32bd
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-16 12:01:52 +0100

    Initial commit

---
+++ src/main/java/com/ynovb3/demoapi/DemoapiApplication.java
@@ -0,0 +1,13 @@
+package com.ynovb3.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynovb3/demoapi/DemoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynovb3.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
