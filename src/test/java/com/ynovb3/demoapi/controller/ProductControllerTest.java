9a41f851177c1171fb7920bf0432b000a3664467
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-16 17:26:01 +0100

    Tests

---
+++ src/test/java/com/ynovb3/demoapi/controller/ProductControllerTest.java
@@ -0,0 +1,30 @@
+package com.ynovb3.demoapi.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testGetProduct() throws Exception {
+		mockMvc.perform(get("/product/clement"))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.name", is("clement")))
+		.andExpect(jsonPath("$.description", is("Description of the product")))
+		.andExpect(jsonPath("$.cost", is(Integer.valueOf(10))));	
+	}
+	
+}
