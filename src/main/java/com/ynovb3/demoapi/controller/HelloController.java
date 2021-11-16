78652079e9c5768c766d6f14e68583869d475f61
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-16 14:53:46 +0100

    Post and Delete

--- src/main/java/com/ynovb3/demoapi/controller/HelloController.java
+++ src/main/java/com/ynovb3/demoapi/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.ynovb3.demoapi.controller;
 
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 //Auto complétion CTRL + ESPACE
 
@@ -10,14 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	Logger logger = org.slf4j.LoggerFactory.getLogger(HelloController.class);
+	
 	//Spécifier l'URL - ici requête GET
+	//récupération de tous
 	@GetMapping("/helloworld")
 	public String getHelloWorld() {
 		return "Hello World";
 	}
 	
+	//Récupération de 1
 	@GetMapping("/helloworld/{str}")
 	public String getHelloWorldWithParam(@PathVariable("str") String str) {
 		return "Get the value : " + str;
+	}
+	
+	//Envoie de 1
+	@PostMapping("/helloworld")
+	public void postHelloWorld(@RequestBody String str) {
+		//Générer des traces dans la console
+		logger.info("Received : " + str);
+	}
+	
+	//Suppression de 1
+	@DeleteMapping("/helloworld/{id}")
+	public void deleteHelloWorld(@PathVariable("id") Integer id) {
+		logger.info("DeleteHelloWorld endpoint called for id : " + id);
 	}
 }
